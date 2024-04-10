e0748884eafe432fadc97ec131edf6d8ee17f844
Mikael Issa <mikael.issa98@hotmail.com>
2024-04-10 15:21:14 +0200

    såå nu har jag ändrat i programm och startup så jag inte får error i terminalen

--- ImageStorageMicroservice/Program.cs
+++ ImageStorageMicroservice/Program.cs
@@ -3,16 +3,17 @@ using ImageStorageMicroservice.Services;
 
 public class Program
 {
-    // Används för att konfigurera och starta värdprocessen för din ASP.NET Core-applikation. Det innehåller allt som behövs för att köra din applikation.
     public static void Main(string[] args)
     {
         CreateHostBuilder(args).Build().Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>()
+                      .UseUrls("https://localhost:5284")
+                      .UseWebRoot("C:\\Utvecklare\\ImageStorageMicroservice\\wwwroot");
+        });
 }
--- ImageStorageMicroservice/Startup.cs
+++ ImageStorageMicroservice/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using ImageStorageMicroservice.Data;
 using ImageStorageMicroservice.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace ImageStorageMicroservice
 {
@@ -18,6 +19,23 @@ namespace ImageStorageMicroservice
             // Lägg till Entity Framework DbContext som en tjänst
             services.AddDbContext<ImageDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Lägg till autentiseringstjänster
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                // Konfigurera JwtBearer-options här, t.ex. hur du validerar JWT-token, tokenutgångstid etc.
+            });
+
+            // Lägg till behörighetskontrolltjänster
+            services.AddAuthorization();
+
+            // Lägg till controllers-tjänster
+            services.AddControllers();
 
             // Lägg till andra tjänster och konfigurationer här
         }
@@ -44,6 +62,7 @@ namespace ImageStorageMicroservice
 
             app.UseRouting();
 
+            app.UseAuthentication(); // Lägg till autentisering middleware
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -55,3 +74,56 @@ namespace ImageStorageMicroservice
         }
     }
 }
+
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+
+//        public IConfiguration Configuration { get; }
+
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            // Lägg till Entity Framework DbContext som en tjänst
+//            services.AddDbContext<ImageDbContext>(options =>
+//                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+//            // Lägg till andra tjänster och konfigurationer här
+//        }
+
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            /* Här konfigureras middleware-kedjan för att hantera HTTP-begäranden. 
+//             * Beroende på om applikationen körs i utvecklings- eller produktionsläge, används olika typer av felhantering och säkerhetsfunktioner. 
+//             * Slutpunkterna för rutning sätts också upp här för att bestämma hur inkommande HTTP-begäranden ska hanteras och vilken kontroller och åtgärder som ska köras.
+//             * I detta fall finns det en standardruta som leder till HomeController's Index-åtgärd om ingen annan matchning hittas.
+//             */
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Home/Error");
+//                app.UseHsts();
+//            }
+
+//            app.UseHttpsRedirection();
+//            app.UseStaticFiles();
+
+//            app.UseRouting();
+
+//            app.UseAuthorization();
+
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllerRoute(
+//                    name: "default",
+//                    pattern: "{controller=Home}/{action=Index}/{id?}");
+//            });
+//        }
+//    }
+//}
